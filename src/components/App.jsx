8ef264b94cfabb749e6cd0718f18d2fef96a1cc6
GraydonHall42 <43125094+GraydonHall42@users.noreply.github.com>
2021-02-23 14:37:28 -0700

    completed module

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 
 function App() {
+  const [headingText, setHeadingText] = React.useState("Hello");
+  const [buttonBackground, setbuttonBackground] = React.useState("White");
+
+  function handleClick() {
+    setHeadingText("submitted");
+  }
+
+  function handleMouseOver() {
+    setbuttonBackground("Black");
+  }
+
+  function handleMouseOver() {
+    setbuttonBackground("Black");
+  }
+
+  function handleMouseOut() {
+    setbuttonBackground("White");
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: buttonBackground }}
+        onMouseOver={handleMouseOver}
+        onClick={handleClick}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
